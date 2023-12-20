cae86a23177011d06e92f9ab2334ebe41fa19507
ekaterinakur <ekaterina1.kur@gmail.com>
2023-12-20 23:53:45 +0200

    No command exception

--- utils/input_handlers.py
+++ utils/input_handlers.py
@@ -3,6 +3,8 @@ from constants.messages import error_messages, command_messages
 
 @input_error(error_messages["no_command"])
 def parse_input(user_input):
+    if not user_input:
+        raise ValueError()
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
