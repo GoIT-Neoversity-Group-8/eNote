e6294baeabdf389ad7babc3831992d32145fd599
preart81 <preart81@gmail.com>
2023-12-22 23:39:21 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eNote",
+    version="0.1",
+    author="Transcarpathian Raccoon",
+    url="https://github.com/GoIT-Neoversity-Group-8/eNote",
+    packages=find_packages(),
+    install_requires=["tabulate", "prompt_toolkit"],
+    entry_points={
+        "console_scripts": [
+            "enote = eNote.main:enot",
+        ],
+    },
+)
